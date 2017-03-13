8f58b96eca36d4e3cc567a961b057dbb2ce54293
kylelingat <kylejlingat@gmail.com>
2017-03-13 08:29:26 -1000

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,5 @@
-console.log('check');
+var idElement = document.getElementById('use-id-method');
+console.log(idElement)
+//finding html element
+var tagElement = document.getElementsByTagName('div');
+console.log(tagElement)
